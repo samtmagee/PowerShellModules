28bdbd05cd4d35ff5d53a1d3e2dd6e9350184190
Sam Magee <sam_t_magee@hotmail.com>
2019-03-06 09:26:04 +0000

    Create Set-WindowsProductKey.ps1
    Funtion to set Windows product keys locally and remotely.

---
+++ Set-WindowsProductKey/Set-WindowsProductKey.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.Synopsis
+   Change Windows product key on remote computer
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Set-WindowsProductKey -ProductKey "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+   This will set the local machines product key.
+.EXAMPLE
+   Set-WindowsProductKey -ComputerName "bob-desktop" -ProductKey "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+   This will set the product key on the computer bob-desktop.
+#>
+function Set-WindowsProductKey
+{
+    [CmdletBinding()]
+    Param
+    (
+        # ComputerName
+        [Parameter]
+        $ComputerName="localhost",
+
+        # ProductKey
+        [Parameter(Mandatory=$true)]
+        $ProductKey
+    )
+
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock { slmgr -ipk $ProductKey }
+}
