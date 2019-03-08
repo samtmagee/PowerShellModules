142966ae00ef2e5b85bd71613a75716d2ecfd7a4
Sam Magee <sam_t_magee@hotmail.com>
2019-03-08 14:29:23 +0000

    Update Get-BitLockerKeys.psm1
    Readded the $jobdata = receive-job $job line that I removed.
    
    Moved the return at the end and sorted/reformatted the output.
    
    Changed test line at the bottom to test two machines.

--- Get-BitLockerKeys/Get-BitLockerKeys.psm1
+++ Get-BitLockerKeys/Get-BitLockerKeys.psm1
@@ -48,23 +48,25 @@ function Get-BitLockerKeys
 
     $counterchecks = 0
     while ((Get-Job).count -ne 0 -and $counterchecks -lt 60){
-            foreach( $job in Get-Job) {
-                if ($job.State -eq [System.Management.Automation.JobState]::Completed) {
-                    Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Saved"
-                    Remove-Job $job;
-                } elseif ($job.State -eq [System.Management.Automation.JobState]::Failed) {
-                    Remove-Job $job;
-                    Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Failed"
-                } elseif ($job.State -eq [System.Management.Automation.JobState]::Disconnected) {
-                    Stop-Job $job;
-                    Remove-Job $job;
-                    Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Disconnected"
-                }
+        foreach( $job in Get-Job) {
+            if ($job.State -eq [System.Management.Automation.JobState]::Completed) {
+                $jobdata = Receive-Job $job
+                Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Saved"
+                Remove-Job $job;
+            } elseif ($job.State -eq [System.Management.Automation.JobState]::Failed) {
+                Remove-Job $job;
+                Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Failed"
+            } elseif ($job.State -eq [System.Management.Automation.JobState]::Disconnected) {
+                Stop-Job $job;
+                Remove-Job $job;
+                Write-Host "$(Get-Date -UFormat `"%Y/%m/%d %H:%M`") - $($job.Location) Disconnected"
             }
-            $counterchecks ++
-            Write-Host $counterchecks
-            sleep -Seconds 1
+        }
+        $counterchecks ++
+        Write-Host $counterchecks
+        sleep -Seconds 1
+
+        return $jobdata | sort ComputerName,MountPoint | ft -AutoSize Date,ComputerName,MountPoint,KeyProtectorType,KeyProtectorId,RecoveryPassword
     }
-    return $jobdata
 }
-Get-BitLockerKeys -ComputerName smagee-pc
+Get-BitLockerKeys -ComputerName "smagee-pc","jkirk-desktop"
