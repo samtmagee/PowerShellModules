2cddf9d37f06488ddb58b7176cc6ce7f21982bc3
Sam Magee <sam_t_magee@hotmail.com>
2019-03-13 16:06:52 +0000

    Create Get-BIOSInfo.psm1
    Initial file

---
+++ Get-BIOSInfo/Get-BIOSInfo.psm1
@@ -0,0 +1,7 @@
+﻿function Get-BIOSInfo {
+    [cmdletbinding()]
+    param (
+    [string[]]$ComputerName = "localhost"
+    )
+        Get-CimInstance Win32_bios -Property * -ComputerName $ComputerName | sort PSComputerName | ft -AutoSize PSComputerName,Manufacturer,SMBIOSBIOSVersion,SerialNumber
+}
