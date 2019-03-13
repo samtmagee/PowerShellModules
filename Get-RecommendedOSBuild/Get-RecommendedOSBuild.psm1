919a405e4635e0ea3822fc7e93d4007ddc9df4c4
Sam Magee <sam_t_magee@hotmail.com>
2019-03-13 16:12:15 +0000

    Create Get-RecommendedOSBuild.psm1

---
+++ Get-RecommendedOSBuild/Get-RecommendedOSBuild.psm1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+   Get the current recommended build version for Windows 10.
+.DESCRIPTION
+   Get the current recommended build version for Windows 10 from the following website
+   https://docs.microsoft.com/en-us/windows/windows-10/release-information
+
+.EXAMPLE
+   Get-RecommendedOSBuild
+#>
+function Get-RecommendedOSBuild
+{
+    [CmdletBinding()]
+    $HTML = Invoke-WebRequest -Uri “https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html“
+    $suggested = ($HTML.ParsedHtml.getElementsByTagName(‘tr’) | Where {$_.className -eq ‘highlight’ }).innerHTML
+    $lines = $suggested.Split([Environment]::NewLine)
+    $linehtml = $lines | select -First 7 | select -Last 1
+    $linehtml = $linehtml -replace "<td>","" -replace "</td>",""
+    return $linehtml
+}
