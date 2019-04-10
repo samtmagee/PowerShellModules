8b4a10f8d9bee728578475662fb7a97ba375a591
Matthew Carter <mcarter@isleworthsyon.org>
2019-04-10 15:32:52 +0100

    Fixed capitalization of varible names and changed return type to void

--- Send-WOL/Send-WOL.psm1
+++ Send-WOL/Send-WOL.psm1
@@ -12,17 +12,17 @@ function Send-WOL
 {
     [CmdletBinding()]
     [Alias()]
-    [OutputType([int])]
+    [OutputType([void])]
     Param
     (
         # The mac address of the computer to wake
         [Parameter(Mandatory=$true,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        [string]$address,
+        [string]$Address,
 
         # The broadcast address for the subnet
-        [string]$broadcast = "10.136.135.255"
+        [string]$Broadcast = "10.136.135.255"
     )
 
     Begin
@@ -31,7 +31,6 @@ function Send-WOL
     Process
     {
         # parse
-        $_broadcast = [Net.IPAddress]::Parse($broadcast);
         $_MAC = [System.Net.NetworkInformation.PhysicalAddress]::Parse($address);
         
         # create the WOL magic packet
@@ -39,7 +38,7 @@ function Send-WOL
         [byte[]]$_packet = (255, 255, 255, 255, 255, 255) + ( $_MAC.GetAddressBytes() * 16 );
         
         # data, length, ip address, port
-        [System.Net.Sockets.UdpClient]::new().Send($_packet, 102, $_broadcast, 9);
+        [void][System.Net.Sockets.UdpClient]::new().Send($_packet, 102, [ipaddress]$Broadcast, 9);
     }
     End
     {
